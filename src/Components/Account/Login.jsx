2ca975c1ed7d5a453936c98c2a8eaf42f68aed33
Mattia <44345310+zMattyPower@users.noreply.github.com>
2024-05-20 13:45:41 +0200

    Added Login system

---
+++ src/Components/Account/Login.jsx
@@ -0,0 +1,251 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock, faEye } from '@fortawesome/free-solid-svg-icons'
+import './Style.css';
+
+const Login = () => {
+  return (
+    <div class="wrapper" id="wrapper">
+        <form name="frmLogin" id="frmLogin" onsubmit="event.preventDefault(); LoginSimulation()">
+            <button type="button" class="backLogin" onclick="showLoginForm()">✕</button>
+            <h1><img src={require("./Login_Icon.png")} alt='login' width="100" draggable="false"/></h1> 
+            <div class="input-box">
+                <FontAwesomeIcon icon={faEnvelope} id="login"/>
+                <input type="email" placeholder="E-Mail" id="email" name="email" required/>
+            </div>
+            
+            <div class="remember-forgot">
+                <a href='about:blank' onclick="showForgotForm()">Password Dimenticata?</a>
+            </div>
+            
+            <div class="input-box">
+                <FontAwesomeIcon icon={faLock} id="login"/>
+                <input type="password" id="password" name="password" ref={passwordRef} placeholder="Password" required/>
+                <FontAwesomeIcon icon={faEye} name="togglePassword" ref={toggleRef} id="togglePassword" onclick="TogglePassword()"/>
+            </div>
+
+            <button type="submit" class="btn">Accedi</button>
+
+            <div class="register-link">
+                <hr/>
+                <p><a href='about:blank' onclick="showRegisterForm()">Crea un account gratuitamente<i class='bx bx-chevrons-right'></i></a></p>
+            </div>
+        </form>
+    </div>
+  );
+};
+
+export default Login
+
+var currentLog = 0;
+var toggleLog = false;
+var toggleReg = false;
+var toggleForgot = false;
+var loggedIn = localStorage['loggedIn'] || "false";
+var loggedEmail = localStorage['loggedEmail'] || null;
+var loggedUsername = localStorage['loggedUsername'] || null;
+
+//Mostra il Form del Login
+function showLoginForm() {
+    if (loggedIn == "true") {
+        alert("Sei loggato come " + loggedUsername + "\nEmail: " + loggedEmail);
+        return;
+    }
+    if (toggleLog) {
+        const BlurredDiv = document.getElementById("blur");
+        BlurredDiv.parentNode.removeChild(BlurredDiv);
+        document.getElementById("wrapper").style.visibility = "hidden";
+        toggleLog = false;
+        currentLog = 0;
+    }
+    else {
+        const BlurredDiv = document.createElement("div");
+        BlurredDiv.id = "blur";
+        document.body.appendChild(BlurredDiv);
+        document.getElementById("wrapper").style.visibility = "visible";
+        toggleLog = true;
+        currentLog = 1;
+    }
+}
+
+//Mostra il Form della Registrazione
+function showRegisterForm() {
+    if (toggleReg) {
+        document.getElementById("wrapper-reg").style.visibility = "hidden";
+        document.getElementById("wrapper").style.visibility = "visible";
+        toggleReg = false;
+        toggleLog = true;
+        currentLog = 1;
+    }
+    else {
+        document.getElementById("wrapper").style.visibility = "hidden";
+        document.getElementById("wrapper-reg").style.visibility = "visible";
+        toggleLog = false;
+        toggleReg = true;
+        currentLog = 2;
+    }
+}
+
+//Mostra il Form per il Reset della Password
+/*function showForgotForm() {
+    if (toggleForgot) {
+        document.getElementById("wrapper-forgot").style.visibility = "hidden";
+        document.getElementById("wrapper").style.visibility = "visible";
+        toggleForgot = false;
+        toggleLog = true;
+        currentLog = 1;
+    }
+    else {
+        document.getElementById("wrapper").style.visibility = "hidden";
+        document.getElementById("wrapper-forgot").style.visibility = "visible";
+        toggleLog = false;
+        toggleForgot = true;
+        currentLog = 3;
+    }
+}*/
+
+//Aggiunta la possibilità di chiudere il form del login premendo Escape
+/*document.addEventListener('keydown', function(event) {
+    if(event.key == 'Escape') {
+        if (currentLog != 0) {
+            closeForm();
+        }
+    }
+});*/
+
+/*function TogglePassword() {
+    const passwordRef = React.useRef();
+    const toggleRef = useRef(null);
+    //const password = document.querySelector("#password");
+    React.useEffect(() => {
+        const password = passwordRef.current;
+        const toggle = toggleRef.current;
+        if (!toggle) return;
+        if (!password) return;
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        toggle.classList.toggle("bx-show");
+    });    
+}*/
+
+function togglePasswordReg() {
+    const password = document.querySelector("#password2");
+    const toggle = document.querySelector("#togglePassword2");
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    toggle.classList.toggle("bx-show");
+}
+
+//Chiude il form corrente, viene usato quando si preme il tasto X o quando si preme Escape con la tastiera
+function closeForm() {
+    const BlurredDiv = document.getElementById("blur");
+    BlurredDiv.parentNode.removeChild(BlurredDiv);
+    if (currentLog == 1) {
+        document.getElementById("wrapper").style.visibility = "hidden";
+        toggleLog = false;
+    }
+    if (currentLog == 2) {
+        document.getElementById("wrapper-reg").style.visibility = "hidden";
+        toggleReg = false;
+    }
+    if (currentLog == 3) {
+        document.getElementById("wrapper-forgot").style.visibility = "hidden";
+        toggleForgot = false;
+    }
+    currentLog = 0;
+}
+
+//Consente di aprire il menu quando la finestra viene rimpicciolita
+function toggleMenu() {
+    let menu = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".navbar");
+
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("open");
+}
+
+function LoginSimulation()
+{
+    var email = document.getElementsByName("email");
+    var user = "null";
+    var pass = document.getElementsByName("password");
+
+    if (email[0].value == "mario.rossi@gmail.com" && pass[0].value == "ciao123") {
+        alert("Login Effettuato Correttamente");
+        loggedIn = "true";
+        user = "Mario";
+        var log = document.getElementById("userLogin");
+        log.innerHTML = "<i class='bx bxs-user'></i>" + user;
+        loggedEmail = email[0].value;
+        loggedUsername = "Mario";
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("loggedUsername", loggedUsername);
+        localStorage.setItem("loggedEmail", loggedEmail);
+        document.getElementById("logout").style.display = "block";
+        document.getElementById("carrello").style.display = "block";
+        closeForm();
+    }
+    else {
+        alert("Password Errata");
+    }
+}
+
+function RegisterSimulation()
+{
+    var email = document.getElementsByName("email2");
+    var user = document.getElementsByName("username");
+    var pass = document.getElementsByName("password2");
+
+    alert("Email: " + email[0].value + "\nUsername: " + user[0].value + " \nPassword: " + pass[0].value);
+    loggedIn = "true";
+    var log = document.getElementById("userLogin");
+    log.innerHTML = "<i class='bx bxs-user'></i>" + user[0].value;
+    loggedEmail = email[0].value;
+    loggedUsername = user[0].value;
+    localStorage.setItem("loggedIn", "true");
+    localStorage.setItem("loggedUsername", loggedUsername);
+    localStorage.setItem("loggedEmail", loggedEmail);
+    document.getElementById("logout").style.display = "block";
+    document.getElementById("carrello").style.display = "block";
+    closeForm();
+}
+
+function ForgotSimulation()
+{
+    var email = document.getElementsByName("email3");
+
+    alert("Email: " + email[0].value);
+    closeForm();
+}
+
+function isLogged() {
+    if (loggedIn == "true") {
+        var log = document.getElementById("userLogin");
+        log.innerHTML = "<i class='bx bxs-user'></i>" + loggedUsername;
+        document.getElementById("logout").style.display = "block";
+        document.getElementById("carrello").style.display = "block";
+    }
+}
+
+/*function isLoggedCarrello() {
+    if (loggedIn == "true") {
+        var log = document.getElementById("userLogin");
+        log.innerHTML = "<i class='bx bxs-user'></i>" + loggedUsername;
+        document.getElementById("logout").style.display = "block";
+        document.getElementById("carrello").style.display = "block";
+    }
+    else {
+        location.replace("./Home.html");
+    }
+}*/
+
+/*function logout() {
+    localStorage.clear();
+    alert("Logout Effettuato Correttamente")
+    location.reload();
+}*/
+
+function messaggioContatti() {
+    alert("Messaggio Inviato, ti risponderemo il prima possibile!\nNOTA: Dacci fino a 72 ore lavorative, sennò inviate un altro messaggio.")
+}
