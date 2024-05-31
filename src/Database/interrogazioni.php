91e85af1b4e5c9dacacc702d8f20a743337fb07f
Adriano995 <adriano.incognito@icloud.com>
2024-05-31 12:21:26 +0200

    ds

--- src/Database/interrogazioni.php
+++ src/Database/interrogazioni.php
@@ -52,4 +52,49 @@ try {
     // Messaggio di errore
     echo json_encode(["message" => "Errore: " . $e->getMessage()]);
 }
+
+try {
+    // Connessione al database
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Ricezione dei dati dal corpo della richiesta
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if ($data) {
+        // Estrazione dei dati
+        $email = $data['email'];  
+        $numero = $data['numero'];      
+        $scadenza = $data['scadenza'];
+        $nome = $data['nome'];
+        $cognome = $data['cognome'];
+        $debito = $debito['debito'];
+        $credito = $credito['credito'];
+        $prepagata = $prepagata['prepagata'];
+        $trattamento = isset($data['trattamento']) && $data['trattamento'] == 'on' ? 1 : 0;
+
+        // Preparazione della query di inserimento
+        $stmt = $conn->prepare("INSERT INTO t_utente (email, numero, scadenza, nome, cognome, debito, credito, prepagata) 
+                                VALUES (:email, :numero, :scadenza, :nome, :cognome, :debito , :credito, :prepagata)");
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':numero', $numero);
+        $stmt->bindParam(':scadenza', $scadenza);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':cognome', $cognome);
+        $stmt->bindParam(':debito', $debito);
+        $stmt->bindParam(':credito', $credito);
+        $stmt->bindParam(':prepagata', $prepagata);
+
+        // Esecuzione della query
+        $stmt->execute();
+
+        // Messaggio di successo
+        echo json_encode(["message" => "Dati inseriti con successo"]);
+    } else {
+        echo json_encode(["message" => "Nessun dato ricevuto"]);
+    }
+} catch (PDOException $e) {
+    // Messaggio di errore
+    echo json_encode(["message" => "Errore: " . $e->getMessage()]);
+}
 ?>
