18c8092ff52aeb02313c49d4c8fc3832be0b1aec
Adriano995 <adriano.incognito@icloud.com>
2024-06-05 12:44:25 +0200

    Aggiunta DB carta
    Prima parte di acquisizione carta, fare controlli successivamente
    Creazione tabella per riempimento magazzino da fare con cicli (php-sql)

---
+++ src/Database/Carta.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+
+include_once "core.php";
+
+$conn = connectDatabase();
+
+$carta_numero_carta = filter_input(INPUT_POST, 'numero_carta', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$carta_scadenza = filter_input(INPUT_POST, 'scadenza', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$carta_titolare = filter_input(INPUT_POST, 'titolare', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$carta_citta = filter_input(INPUT_POST, 'citta', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$carta_cap = filter_input(INPUT_POST, 'cap', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$carta_indirizzo = filter_input(INPUT_POST, 'indirizzo', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$carta_nazione = filter_input(INPUT_POST, 'nazione', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+//fare controlli
+
+?>
