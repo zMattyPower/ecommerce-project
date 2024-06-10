c4e7d016679c678f0d470d3e4e7442a013a07749
Adriano995 <adriano.incognito@icloud.com>
2024-06-10 12:11:33 +0200

    spostamento

--- src/php/login.php
+++ src/php/login.php
@@ -1,6 +1,7 @@
 <?php
 	header('Access-Control-Allow-Origin: http://localhost:8000/progetto_scuola');
-
+	header('Content-Type: application/json');
+	
 	include "core.php";
 
 	// Connecting to Database
