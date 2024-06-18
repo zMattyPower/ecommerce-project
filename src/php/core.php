95efc85382864745d60f95904e7c9ecaf70d8547
micole03 <msantos@studenti.laziodigital.it>
2024-06-18 10:22:05 +0200

    docker

--- src/php/core.php
+++ src/php/core.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: http://localhost:3000');
 
 function connectDatabase() {
-	$servername = "10.104.120.146";
+	$servername = "db";
 	$username = "root";
 	$password = "";
 	$dbname = "progetto_scuola";
